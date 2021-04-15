330ee98ec8a9ca3c08f20cd4c9550068e5f667c6
VadimChorrny <chorrny228@gmail.com>
2021-04-15 20:01:27 +0300

    Добавьте файлы проекта.

---
+++ Gallery/Form2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gallery
+{
+    public partial class Form2 : Form
+    {
+        public int counter;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            counter++;
+            label2.Text = counter.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            counter--;
+            label1.Text = counter.ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Form form = new Form();
+            form.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Gallery/Form3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gallery
+{
+    public partial class Form3 : Form
+    {
+        public double size;
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            size++;
+            this.Opacity = size;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            size--;
+            this.Opacity = size;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
